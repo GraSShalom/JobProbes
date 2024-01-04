295ca1748bb01b8f59c52a73ed092f39fcdab92a
Shalom Grapain <s.grapai@a1qa.com>
2024-01-04 17:39:43 -0600

    Source code for jenkins test job.

---
+++ JobProbes/JobProbes/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace JobProbes
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine("JobProves test from github is running.");
+            Assert.Pass();
+        }
+    }
+}
